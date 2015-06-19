32609b5c41021f013bc674adcecc5040890ddbf5
yusenlee <yusenlee@icloud.com>
2015-06-19 08:00:08 +0800

    patch test

--- EmAop/src/em/aop/Test.java
+++ EmAop/src/em/aop/Test.java
@@ -2,21 +2,21 @@ package em.aop;
 
 public class Test {
 
-	public static void main(String[] args) 
-	 { 
-	 Db d = DbProxyFactory.getAuthInstance("unauth"); 
-	 // 做些事情
-	 d.setMe("do sth");
-	 System.out.println();
-	 d = DbProxyFactory.getAuthInstance("auth");
-	 d.connect("now ok");
-	 System.out.println();
-	 d.setMe("no");
-	 
-	 System.out.println();
-	 // 下面继续执行
-	 
-	 System.out.println(d.getClass()); // 不是Db类了，亲
-	 } 
+	public static void main(String[] args) {
+
+		Db d = DbProxyFactory.getAuthInstance("unauth");
+		// 做些事情
+		d.setMe("do sth");
+		System.out.println();
+		d = DbProxyFactory.getAuthInstance("auth");
+		d.connect("now ok");
+		System.out.println();
+		d.setMe("no");
+
+		System.out.println();
+		// 下面继续执行 patch
+
+		System.out.println(d.getClass()); // 不是Db类了，亲
+	}
 
 }
